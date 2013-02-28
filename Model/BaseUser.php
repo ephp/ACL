62a2bbba776f8e23d667fb960075e9c7512625fb
Ephraim <ephraim.pepe@gmail.com>
2013-02-28 17:26:13 +0100

    Add nickname unico

--- Model/BaseUser.php
+++ Model/BaseUser.php
@@ -24,7 +24,7 @@ abstract class BaseUser extends User {
     /**
      * @var string
      *
-     * @ORM\Column(name="nickname", type="string", length=255, nullable=true)
+     * @ORM\Column(name="nickname", type="string", length=255, nullable=true, unique=true)
      */
     protected $nickname;
 
