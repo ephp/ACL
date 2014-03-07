27820b7447c313ab54d033d86ce13dfd622f7149
Ephraim <staf@jf-system.com>
2014-03-07 18:17:41 +0100

    Si riproca

--- Model/Traits/TwitterUser.php
+++ Model/Traits/TwitterUser.php
@@ -7,9 +7,16 @@ trait TwitterUser {
     /**
      * @var string
      * 
-     * @ORM\Column(name="twitter_id", type="string", length=255, nullable=true)
+     * @ORM\Column(name="twitterId", type="string", length=255, nullable=true)
      */
     protected $twitterId;
+
+    /**
+     * @var string
+     * 
+     * @ORM\Column(name="twitter_id", type="string", length=255, nullable=true)
+     */
+    protected $twitter_id;
 
     /**
      * @var string
@@ -25,6 +32,16 @@ trait TwitterUser {
 
     public function setTwitterId($twitterId) {
         $this->twitterId = $twitterId;
+        return $this;
+    }
+    
+    public function getTwitter_id() {
+        return $this->twitter_id;
+    }
+
+    public function setTwitter_id($twitter_id) {
+        $this->twitter_id = $twitter_id;
+        return $this;
     }
 
     public function getTwitterAccessToken() {
@@ -33,6 +50,7 @@ trait TwitterUser {
 
     public function setTwitterAccessToken($twitter_access_token) {
         $this->twitter_access_token = $twitter_access_token;
+        return $this;
     }
 
     public function serializeTwitter() {
